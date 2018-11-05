23bc8f767e4c0677d879e713312b54649b373e8d
Carel Jan Starreveld <carelstarreveld@hotmail.com>
2018-11-06 11:55:03 +1300

    created for loop to generate pixels

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,32 +1,18 @@
 import React from 'react'
 import Pixel from './Pixel'
 
+
+function fillArray(value, length) {
+  const arr = []
+  for (let i = 0; i < length; i++) {
+    arr.push(value)
+  }
+  return arr
+}
+
 class App extends React.Component {
   render () {
-    return (
-      <div>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-        <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
-      </div>
-    )
+    return fillArray(<Pixel/>, 1000)
   }
 }
 
