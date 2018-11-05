963e68ebad6e6354dc97927e125a311742539fab
Carel Jan Starreveld <carelstarreveld@hotmail.com>
2018-11-06 10:47:05 +1300

    created component

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pixel = () => {
+  return (
+    <div style={{fontFamily: 'Times New Roman',
+                backgroundColor: 'HotPink',
+                height: '50px',
+                width: '50px'}}>lalalal
+    </div>
+  )
+}
+
+export default Pixel
