ea2ac7e15689aebf5051d629ff848dc80e437dfd
Kiri Tavaga <kiri.tavaga@icloud.com>
2018-11-06 13:53:10 +1300

    state your business

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
-const Pixel = () => {
-  return (
-    <div style={{
-      width: 100,
-      height: 100,
-      backgroundColor: 'cornflowerblue'
-    }}>
-    </div>
-  )
+class Pixel extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      style: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'cornflowerblue'
+      }
+    }
+  }
+  // this.setState({
+  //   style: style
+  // })
+  render () {
+    return (
+      <div style={this.state.style}>
+      </div>
+    )
+  }
 }
+
 export default Pixel
